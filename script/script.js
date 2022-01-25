1e754530fd959ad670767dcad42c3393367fe54f
Sebastian Linnet <sebastian.linnet@gmail.com>
2022-01-25 13:56:19 +0100

    smooth scroll

--- script/script.js
+++ script/script.js
@@ -64,3 +64,11 @@ carousel(slideIndex -= slideNo);
 }
 
 carousel(slideIndex);
+
+//smooth scroll
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 700);
+    return false;
+});
